1c30f9f8ae1b9ad0f67ee744b785c36c0960391a
itsanushkah <anushkashrivastava.1402@gmail.com>
2025-05-10 04:19:59 -0400

    Fix: bind to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -37,5 +37,8 @@ def index():
 
     return render_template('index.html', weather=weather)
 
+import os
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+
